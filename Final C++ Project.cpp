a9b6229ebb6c249226015aeef0dd75444bc83ce6
Adam Hakimi <111243011+aadaamn@users.noreply.github.com>
2023-01-23 00:06:19 +0800

    Add files via upload

---
+++ Final C++ Project.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <array>
+#include <vector>
+using namespace std;
+
+int main() {
+    int numbers[100] = {1,2,3,4,5};
+
+    // for (int index = 0; index < 5; index++){
+    //     cout << numbers[index];
+    // }
+
+    cout << numbers[0];
+
+    
+}
